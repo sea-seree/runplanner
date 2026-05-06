dfe55f326956e37c6610ed28aad329ebcf9d81eb
sea-seree <sea@odds.team>
2026-05-06 15:05:12 +0700

    Initial Commit

---
+++ RunPlanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RunPlanner
+//
+//  Created by Seree Saengchan on 6/5/2569 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
